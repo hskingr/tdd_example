e98e3dc295aeb8ddb591b42ccadbb5bd09d8d9e8
harry <git@hskr.xyz>
2022-02-24 13:56:40 +0000

    added tests for David

--- src/main/java/xyz/hskr/main/Factorial.java
+++ src/main/java/xyz/hskr/main/Factorial.java
@@ -4,4 +4,11 @@ public class Factorial {
 	public static void main(String[] args) {
 		System.out.println("hello David");
 	}
+
+	public int calculate(int i) {
+		// TODO Auto-generated method stub
+		return x;
+	}
+	
+	
 }
